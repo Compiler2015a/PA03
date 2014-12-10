7635f8325f58cb1a0a6e653c22fd7f4c820998eb
Mor Hoyda <hoyda_mor@yahoo.com>
2014-12-10 18:47:25 +0200

    add static and virtual methods

--- src/IC/SymbolsTable/IDSymbolsKinds.java
+++ src/IC/SymbolsTable/IDSymbolsKinds.java
@@ -4,11 +4,9 @@ public enum IDSymbolsKinds {
 	 //Class, Method, Variable, Field;
 	
 	CLASS("Class"),
-	FIELD("Field"),
 	STATIC_METHOD("Static Method"),
 	VIRTUAL_METHOD("Virtual Method"),
 	METHOD("Method"),
-	PARAMETER("Parameter"),
 	VARIABLE("Variable");
 	
 	 private final String repr;       
